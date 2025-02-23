fed18fcf0bf70c935f08af593044e3ca4e504977
djerbiano <saberghoudi2222@hotmail.fr>
2025-02-23 05:48:11 +0100

    create header & footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,13 @@
-
+import "./styles.scss";
+import Logo from "../../assets/images/logoFooter.png";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <img src={Logo} alt="Logo Kasa" />
+      <p>© 2025 Kasa. All rights reserved</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,22 @@
-
-
+import "./styles.scss";
+import { Link } from "react-router";
+import Logo from "../../assets/images/logoHeader.png";
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <img className="logoHeader" src={Logo} alt="Logo Kasa" />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/a-propos">A propos</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
